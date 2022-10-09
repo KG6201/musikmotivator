e532718b4305c3867fe41e4dfe3f8a6466901069
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-10-09 12:53:29 +0900

    [add] create and route music controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MusicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('tweet', MusicController::class);
 
 Route::get('/', function () {
     return view('welcome');
