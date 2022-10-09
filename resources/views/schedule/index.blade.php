59adf67af6d788e8a7501c09dc4d368e0b808304
ST <st20027802@gmail.com>
2022-10-09 16:34:13 +0900

    schedule indexの見出しを変更しました。

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -14,7 +14,7 @@
           <table class="text-center w-full border-collapse">
             <thead>
               <tr>
-                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">tweet</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">Schedule</th>
               </tr>
             </thead>
             <tbody>
