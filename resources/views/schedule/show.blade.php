834c4b5d231559fb008764498fdb07273d8fad2e
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-10-15 15:26:14 +0900

    [add] create frontend of start action form on the detail of schedule

--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -16,9 +16,23 @@
                 {{$schedule->schedule_title}}
               </p>
             </div>
-            <a href="{{ url()->previous() }}" class="block text-center w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-              Back
-            </a>
+            @include('common.errors')
+            <form class="mb-6" action="{{ route('action.store') }}" method="POST">
+              @csrf
+              <input type="hidden" name="schedule_id" value="{{ $schedule->id }}">
+              <div class="flex flex-col mb-4">
+                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="action">New Action</label>
+                <input class="border py-2 px-3 text-grey-darkest" type="text" name="action" id="action">
+              </div>
+              <div class="flex justify-evenly">
+                <a href="{{ url()->previous() }}" class="block text-center w-5/12 py-3 mt-6 font-medium tracking-widest text-black uppercase bg-gray-100 shadow-sm focus:outline-none hover:bg-gray-200 hover:shadow-none">
+                  Back
+                </a>
+                <button type="submit" class="w-5/12 py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+                  Start Action
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
