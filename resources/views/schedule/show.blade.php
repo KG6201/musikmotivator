ba56a89293e86e49cb9e40b9467e53f7726e3afe
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-10-18 10:40:54 +0900

    [modify] create timestamps when started and finished and validate them

--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -27,11 +27,6 @@
             @include('common.errors')
             <form class="mb-6" action="{{ route('action.act', $schedule->id) }}" method="GET">
               @csrf
-              <input type="hidden" name="schedule_id" value="{{ $schedule->id }}">
-              <div class="flex flex-col mb-4">
-                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="start">actual start</label>
-                <input class="border py-2 px-3 text-grey-darkest" type="datetime-local" name="start" id="start">
-              </div>
               <div class="flex justify-evenly">
                 <a href="{{ url()->previous() }}" class="block text-center w-5/12 py-3 mt-6 font-medium tracking-widest text-black uppercase bg-gray-100 shadow-sm focus:outline-none hover:bg-gray-200 hover:shadow-none">
                   Back
