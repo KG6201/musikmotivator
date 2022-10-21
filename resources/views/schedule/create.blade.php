43e5464533ef7228f302456db9401fe12e2f2cae
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-10-21 23:27:29 +0900

    [modify] post category id when create schedule

--- resources/views/schedule/create.blade.php
+++ resources/views/schedule/create.blade.php
@@ -12,34 +12,20 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
           @include('common.errors')
-          <form class="mb-6" action="{{ route('category.store') }}" method="POST">
-            @csrf
-            <!--<div class="flex flex-col mb-4">
-                 <div class="mb-2 uppercase font-bold text-lg text-grey-darkest"><label for="category">category</label></div>
-                    <div class="mb-2 uppercase font-bold text-lg text-grey-darkest ">
-                      <select name="task">
-                      <option value="study">study</option>
-                      <option value="work" selected>work</option>
-                      <option value="go home">go home</option>
-                      </select>
-                    </div>
-                  </div>-->
-
-                <div class="form-group">
-                    <label for="category_id">{{ __('カテゴリー') }}</label>
-                    <select class="form-control" id="category-id" name="category_id">
-                        @foreach ($categories as $category)
-                        <option value="{{ $category->category_id }}">{{ $category->category_name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-           </form>   
           <form class="mb-6" action="{{ route('schedule.store') }}" method="POST">
             @csrf                 
-                <div class="flex flex-col mb-4">    
-                  <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="schedule_title">description or title</label>
-                  <input class="border py-2 px-3 text-grey-darkest" type="text" name="schedule_title" id="schedule_title">
-                </div>
+            <div class="form-group">
+                <label for="category_id">{{ __('カテゴリー') }}</label>
+                <select class="form-control" id="category-id" name="category_id">
+                    @foreach ($categories as $category)
+                    <option value="{{ $category->category_id }}">{{ $category->category_name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="flex flex-col mb-4">    
+              <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="schedule_title">description or title</label>
+              <input class="border py-2 px-3 text-grey-darkest" type="text" name="schedule_title" id="schedule_title">
+            </div>
             <div class="flex flex-col mb-4">
               <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="start">start</label>
               <input class="border py-2 px-3 text-grey-darkest" type="datetime-local" name="start" id="start">
