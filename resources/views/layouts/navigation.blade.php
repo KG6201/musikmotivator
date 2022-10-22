87c48491e43ba290b08c63ab1a420baabcf8e8ae
kneg <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2022-10-22 14:24:08 +0900

    [add] create missed responsive navigation link of create category

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -136,6 +136,12 @@
       </x-responsive-nav-link>
     </div>
 
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('category.create')" :active="request()->routeIs('category.create')">
+        {{ __('create category') }}
+      </x-responsive-nav-link>
+    </div>
+
     <!-- Responsive Settings Options -->
     <div class="pt-4 pb-1 border-t border-gray-200">
       <div class="flex items-center px-4">
